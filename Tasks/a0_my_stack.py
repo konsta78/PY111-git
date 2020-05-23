69e8ed3e13f4747655e5a4d95ae7fa0a375713f2
Mr. Konsta <panfilov2507@gmail.com>
2020-05-23 11:21:14 +0300

    решение Task1

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -1,44 +1,61 @@
 """
-My little Stack!!!
+My little Stack
 """
 from typing import Any
 
+stack = [1, 3, 5, 7]
+#stack.remove()
 
-def push(elem: Any) -> None:
+def push(stack, elem: Any) -> None:
     """
     Operation that add element to stack
 
     :param elem: element to be pushed
     :return: Nothing
     """
-    print(elem)
-    return None
+    stack.append(elem)
+    #print(elem)
+    return stack
 
 
-def pop() -> Any:
+def pop(stack) -> Any:
     """
     Pop element from the top of the stack. If not elements - should return None.
 
     :return: popped element
     """
-    return None
+    last_elem = stack[-1]
+    stack.pop()
+    return last_elem
 
 
-def peek(ind: int = 0) -> Any:
+def peek(stack, ind: int = 0) -> Any:
     """
     Allow you to see at the element in the stack without popping it.
 
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
-    print(ind)
-    return None
+    if ind <= len(stack):
+        peek = stack[ind]
+    else:
+        peek = None
+    return peek
 
 
-def clear() -> None:
+def clear(stack) -> None:
     """
     Clear my stack
 
     :return: None
     """
-    return None
+    stack = []
+    return stack
+
+push(stack, 9)
+print(stack)
+last_elem = pop(stack)
+print(stack, last_elem)
+print(clear(stack))
+print(peek(stack, 2))
+print(peek(stack, 8))
