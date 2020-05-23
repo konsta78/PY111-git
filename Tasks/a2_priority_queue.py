88feaf54f5059a0fc5626d3271f9c4c1ebb4ece1
Mr. Konsta <panfilov2507@gmail.com>
2020-05-23 13:05:32 +0300

    solution Task a2

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -4,7 +4,9 @@ Priority Queue
 Queue priorities are from 0 to 5
 """
 from typing import Any
+import random as r
 
+queue = [[] for i in range(11)] # priorities are form 0 to 5
 
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
@@ -13,6 +15,8 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
+    global queue
+    queue[priority].append(elem)
     return None
 
 
@@ -22,6 +26,12 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
+    global queue
+    for i in range(len(queue)):
+        if queue[i]:
+            deq = queue[i][0]
+            del(queue[i][0])
+            return deq
     return None
 
 
@@ -32,7 +42,12 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None
+    global queue
+    if queue[priority]:
+        peek_que = queue[priority][ind]
+    else:
+        peek_que = None
+    return peek_que
 
 
 def clear() -> None:
@@ -41,4 +56,17 @@ def clear() -> None:
 
     :return: None
     """
-    return None
+    global queue
+    queue = [[] for i in range(11)]
+    return queue
+
+if __name__ == "__main__":
+    for i in range(10):
+        enqueue(i, r.randint(0,10))
+
+    print(queue)
+    print(dequeue())
+    print(queue)
+    print(peek(0, 4))
+    print(peek(0, 3))
+    print(peek(0, 2))
