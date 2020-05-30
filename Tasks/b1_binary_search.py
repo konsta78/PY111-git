1f39ada3c65ad5bab3e6565177808ff8d6bfd935
Mr. Konsta <panfilov2507@gmail.com>
2020-05-30 12:38:48 +0300

    task_b1

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,6 +1,7 @@
 from typing import Sequence, Optional
+import random
 
-arr_ = []
+arr_ = [random.randint(-10, 10) for _ in range(20)]
 
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
@@ -10,7 +11,26 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    for ind, el in enumerate(arr):
-        if elem == el:
-            return ind
-    return None
+    arr.sort()
+    l = 0
+    r = len(arr)
+
+    while r - l > 1:
+        m = (l + r) // 2
+        if elem > arr[m]:
+            l = m
+        else:
+            r = m
+
+    if elem == arr[l]:
+        return l
+    elif elem == arr[r]:
+        return r
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    a = binary_search(1, arr_)
+    print(arr_)
+    print(1, a)
